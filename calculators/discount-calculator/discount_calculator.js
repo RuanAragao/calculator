707b3b8b7cf3e0dfe0442d82c5114ef363b8dcfe
unknown <anirudhgarg794@gmail.com>
2023-10-23 00:20:33 +0530

    update

---
+++ calculators/discount-calculator/discount_calculator.js
@@ -0,0 +1,19 @@
+
+function f1(){
+    const x = document.getElementById("original").value;
+    const y = document.getElementById("discount").value;
+    
+    const saving =  (0.01*y*x).toFixed(2);
+    const discounted_price= (x - saving).toFixed(2);
+    
+    console.log(saving);
+    
+    document.querySelector("#result").innerHTML=(`You saved Rs ${saving} <br> Discounted Price is Rs ${discounted_price}`);
+} 
+
+function f2(){
+    document.getElementById("original").value="";
+    document.getElementById("discount").value="";
+    document.getElementById("result").innerHTML="";
+}
+
